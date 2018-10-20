d4a534f0f7ce21eb3372be0866804fbcb3633dd1
zen-gineer <jshtaway@gmail.com>
2018-10-20 12:33:40 -0700

    adf

--- cleanData.py
+++ cleanData.py
@@ -1,7 +1,10 @@
 import os
 import re
-replacements = [('\n', ''), ('   ', ' '), ('  ', ' '), ('\"', ''), 
-                ('\.+', '.'), ('\?', '.'), ('!', '.')]
+import collections
+replacements = [('\s+', ' '), ('”', ''), ('“', ''), ('\"', ''), 
+                ('\.+', '.'), ('\?', '.'), ('!', '.'), ('-', ' '), (':', ' '), 
+                (';', ' '), ('/', ' '), ('\\\\', ' '),("'", ''), ('’\w', ''), 
+                ('\(',''), ('\)',''), ('–', ' '), ('‘', ''), ('…',''), ('>',''), (',', '')]
 
 def cleanData(replacements):
     stories = {}
@@ -9,17 +12,23 @@ def cleanData(replacements):
         for name in files: #Only care about the files
             if not re.search('DS_Store', name):
                 with open (os.path.join(root,name), 'r+', encoding="utf-8", errors='ignore') as f:
-                    stories[name.split('.')[0]] = ' '.join(f.readlines()).lower()
+                    stories[name.split('.')[0]] = f.read().lower()#' '.join(f.readlines()).lower()
 
 
     with open(os.path.join('.','data','combinedText.txt'), 'w+') as textfile, \
         open(os.path.join('.','data','combinedText.txt'), 'w+') as meta:
+        alltext = ''
         for story,text in stories.items():
             for cur, new in replacements:
-                #print(story, ': ', cur,' ',new)
+                print(cur,new)
                 text = re.sub(cur, new, text)
+            text = text.replace('...','.').replace('..','.')
             meta.write(story+'\n')
             textfile.write(text + ' ')
+            alltext += text + ' '
+    count = collections.Counter(alltext).most_common()
+    print(alltext)
+    print([c[0] for c in count])
 
 if __name__ == "__main__":
     cleanData(replacements)
